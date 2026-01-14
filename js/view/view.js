f0a48a60469741dc1d80b2e03ac6d0a64057ac64
TimonHu <302384226@student.rocmondriaan.nl>
2026-01-14 13:31:27 +0100

    save

--- js/view/view.js
+++ js/view/view.js
@@ -1,11 +1,30 @@
 'use strict'
-//hier komen de view functies, hieronder staat voorbeeld code
-const buttons = document.querySelectorAll('.letters button');
 
-function bindButtons(){
-    for(let b=0;b<buttons.length;b++)
-    {
-        buttons[b].addEventListener('click', handleLetterClick);
+const body = document.querySelector("body");
+
+function showbord(bord){
+    body.innerHTML = "";
+
+    const bordE = document.createElement("div");
+    bordE.classList.add("container")
+
+    for(let i = 0;i<bord.length;i++){
+        const row = document.createElement("div");
+        row.classList.add("row")
+
+        for(let x = 0;x<bord[i].length;x++){
+            const tile = document.createElement("div");
+            tile.classList.add("col");
+
+            if((i+x)%2===0){
+
+            }
+
+            row.appendChild(tile);
+        }
+        bordE.appendChild(row)
     }
+
+    body.appendChild(bordE);
 }
 
